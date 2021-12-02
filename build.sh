f5223f63807f8b864bc8d1c0864c21afea1f5148
zouyonghao <zouyonghao1994@163.com>
2021-12-02 10:52:41 +0800

    only build with normal asan

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ sudo apt install liblua5.1-dev
 
 make clean
 
-# make USE_LUAJIT=0 \
-#     CC="clang-9 -fsanitize=address"
+make USE_LUAJIT=0 \
+    CC="clang-9 -fsanitize=address"
 
 # make
 
-make USE_LUAJIT=0 CC=/home/zyh/distributed-system-test/build/fuzz/default_compiler
+# make USE_LUAJIT=0 CC=/home/zyh/distributed-system-test/build/fuzz/default_compiler
